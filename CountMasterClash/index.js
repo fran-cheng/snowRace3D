f76ad2dcc420345cb8ec0fcff558cc61a14c59cf
Fran <1093451587@qq.com>
2024-03-10 11:38:55 +0800

    COuntMasterClash提交

---
+++ CountMasterClash/index.js
@@ -0,0 +1,11 @@
+window.screenOrientation = "sensor_landscape", 
+// loadLib("libs/min/laya.core.min.js"), 
+// loadLib("libs/min/laya.ani.min.js"), 
+// loadLib("libs/min/laya.html.min.js"), 
+// loadLib("libs/min/laya.particle.min.js"), 
+// loadLib("libs/min/laya.ui.min.js"), 
+// loadLib("libs/min/laya.d3.min.js"), 
+loadLib("libs/min/laya.device.min.js"), 
+loadLib("libs/min/domparserinone.min.js"), 
+loadLib("libs/min/physic2d.min.js"), 
+loadLib("js/main.js");
